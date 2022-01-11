a5a82280cd83e13f656aa350b179cc264fb48a09
HeonchanKim <khc9274@naver.com>
2022-01-11 17:11:17 +0900

    22_01_11

---
+++ baekjoon/src/Ex/Q06_2742.java
@@ -0,0 +1,25 @@
+package Ex;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Q06_2742 {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bf = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i = N; i > 0 ; i--) {
+			bf.write(i+"\n");
+		}
+		bf.flush();
+		bf.close();
+		br.close();
+		
+	}
+}
