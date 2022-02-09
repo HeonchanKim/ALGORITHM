082fc51b207c3db6e87573cd577d3183101594a7
HeonchanKim <khc9274@naver.com>
2022-02-09 23:46:44 +0900

    22_02_09

--- JavaAlgorithm/src/Array/Q02.java
+++ JavaAlgorithm/src/Array/Q02.java
@@ -3,20 +3,34 @@ package Array;
 import java.util.Scanner;
 
 public class Q02 {
-	public int solution() {
-		int answer = 0;
+	public int solution(int[] arr) {
+		int answer = 1;
+		
+		for (int i = 1; i < arr.length; i++) {
+			boolean c = true;
+			for(int j = 0;j < i;j++) {
+				if(arr[i]<=arr[j]) {
+					c = false;
+					break;
+				}
+			}	
+			
+			if(c) {
+				answer++;
+			}
+		}
 		return answer;
 	}
+	
 	public static void main(String[] args) {
 		Q02 T = new Q02();
 		Scanner kb = new Scanner(System.in);
 		int num = kb.nextInt();
 		int[] arr = new int[num];
 		
-		for (int i = 0; i < arr.length; i++) {
+		for (int i = 0; i < num; i++) {
 			arr[i] = kb.nextInt();
 		}
-		
-		System.out.println(T.solution());
+		System.out.println(T.solution(arr));
 	}
 }
---
+++ JavaAlgorithm/src/Array/Q04.java
@@ -0,0 +1,24 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Q04 {
+	public String solution(int num) {
+		String answer = "";
+		int a = 0;
+		int b = 0;
+		
+		for (int i = 0; i < num; i++) {
+			
+		}
+		
+		return answer;
+	}
+	public static void main(String[] args) {
+		Q04 T = new Q04();
+		Scanner kb = new Scanner(System.in);
+		int num = kb.nextInt();
+		
+		System.out.println(T.solution(num));
+	}
+}
