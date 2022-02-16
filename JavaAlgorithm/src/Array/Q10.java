430e3196d93a4426c3a14001d6264aeee3d9c942
HeonchanKim <khc9274@naver.com>
2022-02-16 23:51:25 +0900

    22_02_16

--- JavaAlgorithm/src/Array/Q10.java
+++ JavaAlgorithm/src/Array/Q10.java
@@ -1,7 +1,20 @@
 package Array;
 
-public class Q10 {
-	public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Q10 {
+	public int solution(int n, int[][] arr) {
+		int answer = 0;
+		
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		Q10 T = new Q10();
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		int[][] arr = new int[n][n];
+		
+		System.out.println(T.solution(n, arr));
 	}
 }
