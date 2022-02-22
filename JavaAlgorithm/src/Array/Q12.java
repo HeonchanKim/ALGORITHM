ef0bea783d296408ff6f5c652f0571e8a6d047ec
HeonchanKim <khc9274@naver.com>
2022-02-22 23:03:07 +0900

    22_02_22

--- JavaAlgorithm/src/Array/Q12.java
+++ JavaAlgorithm/src/Array/Q12.java
@@ -5,6 +5,23 @@ import java.util.Scanner;
 public class Q12 {
 	public int solution(int n, int m, int[][] arr){
 		int answer = 0;
+		
+		for(int i = 1;i <= n;i++){
+			for(int j = 1;j <= n;j++){
+				int cnt = 0;
+				for(int k = 0;k < m;k++){
+					int pi = 0, pj = 0;
+					for(int s = 0;s < n;s++){
+						if(arr[k][s] == i) pi = s;
+						if(arr[k][s] == j) pj = s;
+					}
+					if(pi < pj) cnt++;
+				}
+				if(cnt == m){
+					answer++;
+				}
+			}
+		}
 		return answer;
 	}
 
